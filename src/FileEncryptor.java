be6296c5d88b16f88b150ed70fd2d2f77f2b93b8
Pravin Modotholi <pravinmanoj44@gmail.com>
2021-07-24 20:27:52 +1200

    feat: file paths modified
    Will encrypt and decrypt local files specified in the
    resources directory
    
    BREAKING CHNAGE: no longer creates afiles in the temp directory

--- src/FileEncryptor.java
+++ src/FileEncryptor.java
@@ -1,8 +1,12 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -42,36 +46,41 @@ public class FileEncryptor {
         cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
 
         //Look for files here
-        final Path tempDir = Files.createTempDirectory("packt-crypto");
+        final Path inputFilePath = Paths.get(System.getProperty("user.dir").toString() + "/resources/plaintext.txt");
+        File encryptedFile = new File(System.getProperty("user.dir").toString() + "/resources/ciphertext.enc");
+        if (!encryptedFile.exists()) { encryptedFile.createNewFile(); }
         
-        final Path encryptedPath = tempDir.resolve("1 - Encrypting and Decrypting files.pptx.encrypted");
-        try (InputStream fin = FileEncryptor.class.getResourceAsStream("1 - Encrypting and Decrypting files.pptx");
-                OutputStream fout = Files.newOutputStream(encryptedPath);
+        try (InputStream fin = Files.newInputStream(inputFilePath);
+                OutputStream fout = new FileOutputStream(encryptedFile);
                 CipherOutputStream cipherOut = new CipherOutputStream(fout, cipher) {
         }) {
             final byte[] bytes = new byte[1024];
-            for(int length=fin.read(bytes); length!=-1; length = fin.read(bytes)){
+            for(int length = fin.read(bytes); length != -1; length = fin.read(bytes)){
                 cipherOut.write(bytes, 0, length);
             }
         } catch (IOException e) {
             LOG.log(Level.INFO, "Unable to encrypt", e);
         }
         
-        LOG.info("Encryption finished, saved at " + encryptedPath);
+        LOG.info("Encryption finished, saved at " + encryptedFile);
         
         cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
-        final Path decryptedPath = tempDir.resolve("1 - Encrypting and Decrypting files_decrypted.pptx");
-        try(InputStream encryptedData = Files.newInputStream(encryptedPath);
-                CipherInputStream decryptStream = new CipherInputStream(encryptedData, cipher);
-                OutputStream decryptedOut = Files.newOutputStream(decryptedPath)){
-            final byte[] bytes = new byte[1024];
-            for(int length=decryptStream.read(bytes); length!=-1; length = decryptStream.read(bytes)){
-                decryptedOut.write(bytes, 0, length);
-            }
+        File decryptedFile = new File(System.getProperty("user.dir").toString() + "/resources/decrypted.txt");
+        if (!decryptedFile.exists()) { decryptedFile.createNewFile(); }
+        
+        try(InputStream encryptedData = new FileInputStream(encryptedFile);
+            CipherInputStream decryptStream = new CipherInputStream(encryptedData, cipher);
+            OutputStream decryptedOut = new FileOutputStream(decryptedFile)) {
+            
+                final byte[] bytes = new byte[1024];
+                for(int length=decryptStream.read(bytes); length!=-1; length = decryptStream.read(bytes)){
+                    decryptedOut.write(bytes, 0, length);
+                }
+
         } catch (IOException ex) {
             Logger.getLogger(FileEncryptor.class.getName()).log(Level.SEVERE, "Unable to decrypt", ex);
         }
         
-        LOG.info("Decryption complete, open " + decryptedPath);
+        LOG.info("Decryption complete, open " + decryptedFile);
     }
 }
